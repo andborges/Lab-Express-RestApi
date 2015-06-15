056df4fe6453e98fb504356305453a9635c16810
Andre Borges Medeiros <andborges@gmail.com>
2015-06-14 21:07:39 -0300

    Allow cross-domain requests

--- app.js
+++ app.js
@@ -8,18 +8,26 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Allowing cross-origin requests
+app.use(function(req, res, next) {
+  // console.log('Time:', Date.now());
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api/v1', routesV1);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
+// Error handlers
 
-// development error handler
+// Development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
@@ -32,7 +40,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
+// Production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
--- routes/v1/main.js
+++ routes/v1/main.js
@@ -1,5 +1,5 @@
 module.exports = function(router) {
 	router.get('/', function(req, res, next) {
-	  res.json({ message: 'Version: 0.0.1' });
+	  	res.json({ message: 'Version: 0.0.1' });
 	});
 }
